4d6cbe3654e9783b386559e35c1290ddd2c85bcf
faisalsyah-web <faisalsyahruddin57@gmail.com>
2022-12-17 21:33:06 +0900

    tambah css

--- tahunbaru/views.py
+++ tahunbaru/views.py
@@ -5,6 +5,6 @@ import datetime
 # Create your views here.
 def tahunBaru(request):
     skrng = datetime.datetime.now()
-    return render(request, "page/index.html", {
-        "thnbru": skrng.month == 1 and skrng.day == 1
-    })
+    return render(
+        request, "page/index.html", {"thnbru": skrng.month == 1 and skrng.day == 1}
+    )
